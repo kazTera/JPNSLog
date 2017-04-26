109945656fc044f3e186ff19390606a44ef375b9
Yamamoto Kazunori <kazunori-yamamoto@M0027.local>
2017-04-26 13:24:09 +0900

    add: Carthage

---
+++ Carthage/Build/iOS/JPNSLog.framework/Headers/JPNSLog.h
@@ -0,0 +1,19 @@
+//
+//  JapanNSLog.h
+//  JapanNSLog
+//
+//  Created by Yamamoto Kazunori on 2017/04/26.
+//  Copyright © 2017年 Yamamoto Kazunori. All rights reserved.
+//
+#import <UIKit/UIKit.h>
+#import "JPNSLogDescription.h"
+
+//! Project version number for JapanNSLog.
+FOUNDATION_EXPORT double JapanNSLogVersionNumber;
+
+//! Project version string for JapanNSLog.
+FOUNDATION_EXPORT const unsigned char JapanNSLogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JapanNSLog/PublicHeader.h>
+
+
