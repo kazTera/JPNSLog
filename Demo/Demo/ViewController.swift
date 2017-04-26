1620b47378eee0c7dffc6c60ed3c303594451d80
Yamamoto Kazunori <kazunori-yamamoto@M0027.local>
2017-04-26 12:05:01 +0900

    rename

---
+++ Demo/Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Yamamoto Kazunori on 2017/04/26.
+//  Copyright © 2017年 Yamamoto Kazunori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
